68f9d98b83afa7f8038151c2ac611dc7f06caa8c
Skim53 <skim53@gmu.edu>
2022-11-27 23:31:41 -0500

    third commit

--- prediction.py
+++ prediction.py
@@ -1,5 +1,5 @@
 import joblib
 #function that takes predict() function model to predict based on new DVOA values 
 def predict(data):
-    lr = joblib.load(r"C:\Users\17036\Documents\School\Fall_2022\CDS_303\Code\lr_model.sav")
+    lr = joblib.load(r"C:\Users\17036\Documents\School\Fall_2022\CDS_303\Code\Streamlit\lr_model.sav")
     return lr.predict(data)
